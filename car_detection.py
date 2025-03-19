e506d04e3cbafce2e3f42d71df4ec52ea5407125
CameronPinchin <cameronpinchin@cmail.carleton.ca>
2025-03-18 21:07:35 -0400

    updates to car_detection.py
    - defined ROIs for images taken with the pi-camera
    - added take_picture_and_crop() function

--- car_detection.py
+++ car_detection.py
@@ -1,11 +1,64 @@
 from __future__ import print_function
 from PIL import Image
+from picamzero import Camera
+import os
 import cv2 as cv
 import argparse
 import numpy as np
 import requests
 
-def detectCars(frame):
+
+def take_and_crop_photo():
+
+    # Define length and height of each image
+    length = 2592 
+    height = 1944
+    cropped_height = height * (1/3)
+
+    # Define region of interest values
+    # These will need further definitions and error checking.
+
+    roi_one_x1 = 0
+    roi_one_y1 = 648
+    roi_one_x2 = 864
+    roi_one_y2 = 1296
+
+    roi_two_x1 = 864
+    roi_two_y1 = 648
+    roi_two_x2 = 1728
+    roi_two_y2 = 1296
+
+    roi_three_x1 = 864
+    roi_three_y1 = 1296
+    roi_three_x2 = 2592
+    roi_three_y2 = 1296
+    
+
+    home_dir = os.environ['~']
+    cam = Camera()
+
+    cam.start_preview()
+    cam.take_photo(f"{home_dir}/img/img.jpg")
+    cam.stop_preview()
+
+    img_parent = Image.open(f"{home_dir}/img/img.jpg")
+    img_parent = img_parent.crop((0, cropped_height, length, height))
+
+    img_manipulate = cv2.imread(f"{home_dir}/img/img.jpg")
+
+    # Define ROIs for image
+    roi_parking_spot_one = img_parent[1296:648, 0:864]
+    roi_parking_spot_two = img_parent[1296:648, 1728:]
+    roi_parking_spot_three = img_parent[1296:1296, 864:2592]
+
+    cv2.imshow("Show selected ROI: ", roi_parking_spot_one)
+    cv2.imshow("Show selected ROI: ", roi_parking_spot_two)
+    cv2.imshow("Show selected ROI: ", roi_parking_spot_three)
+
+    
+
+
+def detect_cars(frame):
     # Car Detection Testing with online image
     
     frame = frame.resize((450, 250))
